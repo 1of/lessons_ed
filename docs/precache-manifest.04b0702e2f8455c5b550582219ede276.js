27f17a1d082e667b1a81451f13f8ea5bfee857cd
Alexey Silifonov <silifonov@gmail.com>
2018-11-28 16:19:56 +0200

    test

---
+++ docs/precache-manifest.04b0702e2f8455c5b550582219ede276.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "78de65233e24f6d684fd",
+    "url": "/static/css/main.fd7cc62c.chunk.css"
+  },
+  {
+    "revision": "78de65233e24f6d684fd",
+    "url": "/static/js/main.78de6523.chunk.js"
+  },
+  {
+    "revision": "b71fa89db87feec0f7ef",
+    "url": "/static/css/1.de1211b1.chunk.css"
+  },
+  {
+    "revision": "b71fa89db87feec0f7ef",
+    "url": "/static/js/1.b71fa89d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86f81eb6a97991580e65fb6e92f4e72e",
+    "url": "/static/media/arrow.86f81eb6.svg"
+  },
+  {
+    "revision": "7b1d49aaacad2ec578e367ff6eb11c59",
+    "url": "/static/media/Marketscap-Logo-Horizontal.7b1d49aa.png"
+  },
+  {
+    "revision": "b8db18b54c196cd60728175b933e7f49",
+    "url": "/static/media/visaMC.b8db18b5.png"
+  },
+  {
+    "revision": "58f76e815e1706ae9a09b1dfa3f5377e",
+    "url": "/static/media/cvv.58f76e81.png"
+  },
+  {
+    "revision": "d5032d3f4657ac7624615e46ffb85716",
+    "url": "/static/media/ssl-image.d5032d3f.png"
+  },
+  {
+    "revision": "bd1fa2cb0ee8b48875b07f5e08314b15",
+    "url": "/static/media/credits.bd1fa2cb.png"
+  },
+  {
+    "revision": "056f01c52f26012044432f219cbd1a0d",
+    "url": "/static/media/comodo.056f01c5.png"
+  },
+  {
+    "revision": "93f3f5b9d991527b2ebe4ce4079cbb4d",
+    "url": "/index.html"
+  }
+];
